3bf311fb9c7b94f4e6d14a69fde388e8100196f1
Taufeeq28 <sameerahmed6686@gmail.com>
2024-10-01 17:18:23 -0500

    Create the Review Model

---
+++ MediaReviewHubWeb/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediaReviewHubWeb.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string ReviewText { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateReviewed { get; set; }
+    }
+}
