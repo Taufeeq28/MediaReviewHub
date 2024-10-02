c214dd58b0dac091d426d4b27ad72a695f6ba2a6
Taufeeq28 <sameerahmed6686@gmail.com>
2024-10-01 22:11:16 -0500

    Add Reviews to the list

---
+++ MediaReviewHubWeb/Pages/Reviews/CreateReview.cshtml.cs
@@ -0,0 +1,38 @@
+using MediaReviewHubWeb.Data;
+using MediaReviewHubWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace MediaReviewHubWeb.Pages.Reviews
+{
+    [BindProperties]
+    public class CreateReviewModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Review Review { get; set; }
+
+        public CreateReviewModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            // Convert DateReviewed to UTC
+            if (Review.DateReviewed.Kind == DateTimeKind.Unspecified)
+            {
+                Review.DateReviewed = DateTime.SpecifyKind(Review.DateReviewed, DateTimeKind.Utc);
+            }
+
+            await _db.Reviews.AddAsync(Review);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
