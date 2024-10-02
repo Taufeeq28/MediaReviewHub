eb08ff5107ee16395bc93f0b1208436a88f77299
Taufeeq28 <sameerahmed6686@gmail.com>
2024-10-01 21:02:14 -0500

    Create Reviews List

---
+++ MediaReviewHubWeb/Pages/Reviews/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using MediaReviewHubWeb.Data;
+using MediaReviewHubWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MediaReviewHubWeb.Pages.Reviews
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Review> Reviews { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+            Reviews = _db.Reviews;
+        }
+    }
+}
