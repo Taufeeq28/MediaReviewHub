c22a700d17e2a160970db3f9825e6b6b70a47009
Taufeeq28 <sameerahmed6686@gmail.com>
2024-10-04 13:28:04 -0500

    implement category filter and homepge ui update

--- MediaReviewHubWeb/wwwroot/js/site.js
+++ MediaReviewHubWeb/wwwroot/js/site.js
@@ -1,7 +1,27 @@
 ﻿$(document).ready(function () {
-    $('#myTable').DataTable({
+    // Initialize DataTable
+    var table = $('#myTable').DataTable({
         "scrollY": "900px",
         "scrollCollapse": true,
-        "paging": true
+        "paging": true,
+        
+    });
+
+    // Apply filters automatically on checkbox change
+    $("input[name='categoryFilter']").on('change', function () {
+        var selectedCategories = [];
+
+        // Collect selected categories
+        $("input[name='categoryFilter']:checked").each(function () {
+            selectedCategories.push($(this).val());  // Get all selected categories
+        });
+
+        // Use regex to filter the DataTable based on selected categories
+        if (selectedCategories.length > 0) {
+            var regex = selectedCategories.join('|'); // Create a regex pattern from the selected categories (Movies|Books|Music|Games)
+            table.column(1).search(regex, true, false).draw(); // Filter column 1 (Category column) using regex
+        } else {
+            table.column(1).search('').draw(); // If no category is selected, show all rows
+        }
     });
 });
